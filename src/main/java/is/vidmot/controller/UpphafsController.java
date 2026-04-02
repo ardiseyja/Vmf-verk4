03d418e1c5124e0a9aea076ec81c843096f7d332
Kristín Jónsdóttir <krillajon@gmail.com>
2026-04-02 12:37:17 +0000

    Kasta upp á hver byrjar og binda takka við hvor aðra.

--- src/main/java/is/vidmot/controller/UpphafsController.java
+++ src/main/java/is/vidmot/controller/UpphafsController.java
@@ -2,6 +2,8 @@ package is.vidmot.controller;
 
 import is.vidmot.switcher.View;
 import is.vidmot.switcher.ViewSwitcher;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class UpphafsController {
 
@@ -18,13 +21,21 @@ public class UpphafsController {
     @FXML
     private Button fxByrja;
 
+    @FXML Button fxHverByrjar;
+
     @FXML
     private ComboBox<String> fxVeljaLit;
 
     public String selectedString;
 
+    private static final int MAX = 2;
+    private final IntegerProperty byrjaLeikmadur = new SimpleIntegerProperty(MAX);
+    private final Random randomNum = new Random();
+
     public void initialize() {
         frumstillaLiti();
+        fxHverByrjar.disableProperty().bind(fxVeljaLit.getSelectionModel().selectedItemProperty().isNull());
+        fxByrja.setDisable(true);
     }
 
     private void frumstillaLiti() {
@@ -47,7 +58,12 @@ public class UpphafsController {
 
     @FXML
     private void onHverByrjar(ActionEvent event){
-
+        int byrja = randomNum.nextInt(2);
+        String[] hverByrjarTexti = {fxVeljaLit.getSelectionModel().getSelectedItem() + " byrjar", "Svartur byrjar"};
+        fxHverByrjar.setText(hverByrjarTexti[byrja]);
+        fxHverByrjar.disableProperty().unbind();
+        fxHverByrjar.setDisable(true);
+        fxByrja.setDisable(false);
     }
 
 }
